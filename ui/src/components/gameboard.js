988050564bf6c2dd3a35a325f392ba25b51a76e8
virajshastri97 <viraj.shastri@colostate.edu>
2020-04-20 10:48:00 -0600

    Add base structure

---
+++ ui/src/components/gameboard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class GameBoard extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
