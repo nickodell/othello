77a8c76be10e2f78ea3388e95c78abc5e6715cf5
virajshastri97 <viraj.shastri@colostate.edu>
2020-04-20 22:02:33 -0600

    Add actions

---
+++ ui/src/actions/Actions.js
@@ -0,0 +1,21 @@
+import { UPDATE_GAMEBOARD, GET_LEGAL_MOVES } from './types';
+
+export const getGamestate = (newMove) => dispatch => {
+    const newGamestate = await fetch(''); // replace with web3 call
+    const parsedGamestate = await newGamestate.json();
+
+    dispatch({
+        type: UPDATE_GAMEBOARD,
+        payload: parsedGamestate
+    });
+};
+
+export const getLegalMoves = () => dispatch => {
+    const legalMoves = await fetch(''); // replace with web3 call
+    const parsedLegalMoves = await legalMoves.json();
+
+    dispatch({
+        type: GET_LEGAL_MOVES,
+        payload: parsedLegalMoves
+    });
+};
