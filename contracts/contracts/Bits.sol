967da353f469d44a7ff3c1acd66d4d64ef40578d
Nick ODell <nickodell@gmail.com>
2020-04-24 16:59:09 -0600

    Fix bug: do mask before shifting

--- contracts/contracts/Bits.sol
+++ contracts/contracts/Bits.sol
@@ -97,7 +97,7 @@ library Bits128 {
     function setBits(uint128 self, uint8 startIndex, uint16 numBits, uint128 newValue) internal pure returns (uint128) {
         require(0 < numBits && startIndex < 128 && startIndex + numBits <= 128);
         uint128 maskInv = (uint128(1) << numBits) - 1;
-        uint128 newValueShifted = (newValue << startIndex) & maskInv;
+        uint128 newValueShifted = (newValue & maskInv) << startIndex;
         uint128 mask = ~maskInv;
         uint128 maskShifted = mask << startIndex;
         uint128 selfCleared = maskShifted & self;
