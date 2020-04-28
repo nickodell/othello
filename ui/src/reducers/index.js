ef28e664df4737b2089fe5b06f2c4a4f1c31833a
virajshastri97 <viraj.shastri@colostate.edu>
2020-04-27 22:07:31 -0600

    Add modal reducer

--- ui/src/reducers/index.js
+++ ui/src/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 
 import gameReducer from './gameReducer';
 import web3Reducer from './web3Reducer';
+import modalReducer from './modalReducer';
 
 export default combineReducers({
     game: gameReducer,
-    web3: web3Reducer
+    web3: web3Reducer,
+    modal: modalReducer
 });
