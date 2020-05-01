16a4ec8e877ad427240814f1640dd6a49cda310c
virajshastri97 <viraj.shastri@colostate.edu>
2020-04-30 20:30:07 -0600

    Save contract to state

--- ui/src/containers/web3container.js
+++ ui/src/containers/web3container.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-import { getWeb3Instance } from '../actions/Actions';
+import { getWeb3Instance, getContracts } from '../actions/Actions';
 
 class Web3Container extends Component {
     async componentDidMount() {
         await this.props.getWeb3Instance();
         console.log('Account: ' + this.props.account);
-        console.log('Network ID: ' + this.props.networkId);
+        await this.props.getContracts(this.props.web3);
     }
     render() {
         return null;
@@ -17,13 +17,16 @@ class Web3Container extends Component {
 
 Web3Container.propTypes = {
     getWeb3Instance: PropTypes.func.isRequired,
+    getContracts: PropTypes.func.isRequired,
+    web3: PropTypes.object,
     account: PropTypes.string,
-    networkId: PropTypes.number
+    ofContract: PropTypes.object
 };
 
 const mapStateToProps = (state) => ({
+    web3: state.web3.web3,
     account: state.web3.account,
-    networkId: state.web3.networkId
+    ofContract: state.web3.ofContract
 });
 
-export default connect(mapStateToProps, { getWeb3Instance })(Web3Container);
+export default connect(mapStateToProps, { getWeb3Instance, getContracts })(Web3Container);
