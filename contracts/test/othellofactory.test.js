82a8d43f85450b375c23cb155cd3727fa27bdfdb
tejaswini4 <lizacool.612@gmail.com>
2020-05-02 15:43:49 -0600

    added-few-tests

--- contracts/test/othellofactory.test.js
+++ contracts/test/othellofactory.test.js
@@ -4,7 +4,7 @@ const BLACK = "1";
 const WHITE = "3";
 
 contract("Factory", async accounts => {
-    let [alice, bob] = accounts;
+    let [alice, bob, eve, john] = accounts;
     let factory;
     it("should create a new game when two players are there", async () => {
         factory = await Factory.deployed();
@@ -16,13 +16,20 @@ contract("Factory", async accounts => {
         assert.equal(result.logs[0].args[1],alice);
     });
     it("getMyColor is working fine", async () => {
-        factory = await Factory.deployed();
-        await factory.createNewGame("alice", {from: alice});
-        await factory.createNewGame("bob", {from: bob});
+        // factory = await Factory.deployed();
         const bobColor= await factory.getMyColor({from:bob});
         const aliceColor= await factory.getMyColor({from:alice});
-        console.log(aliceColor);
+        // console.log(aliceColor);
         assert.equal(aliceColor,"WHITE");
         assert.equal(bobColor,"BLACK");
     });
+    it("getOpponent is working fine", async()=>{
+        const opponent = await factory.getMyOpponent({from:bob});
+        assert.equal(opponent,alice);
+    })
+    it("getCurrentState is working fine", async()=>{
+        const state = await factory.getCurrentState({from:eve});
+        assert.equal(state,"IDLE");
+    })
+
 })
