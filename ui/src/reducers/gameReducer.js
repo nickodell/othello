095c9deb9125207be6b835d1289c5fecc5ca56a1
virajshastri97 <viraj.shastri@colostate.edu>
2020-05-03 01:50:35 -0600

    Remove myTurn from legal moves reducer

--- ui/src/reducers/gameReducer.js
+++ ui/src/reducers/gameReducer.js
@@ -21,16 +21,9 @@ export default function (state=initialState, action) {
                 gamestate: action.payload
             };
         case GET_LEGAL_MOVES:
-            let myTurn = false;
-            if (((state.myColor === 'WHITE') && (action.payload.whiteToMove)) || ((state.myColor === 'BLACK') && (!action.payload.whiteToMove))) {
-                myTurn = true;
-            }
-            // console.log("action.payload", action.payload)
-            // console.log("validMoves", action.payload.validMoves)
             return {
                 ...state,
-                legalMoves: action.payload,
-                myTurn: myTurn
+                legalMoves: action.payload
             };
         case PLAY_MOVE:
             return {
